c1c4a825b99e088d65be1b82f3e083e12f021511
harzinn <nickkrugz@gmail.com>
2021-09-17 15:25:24 -0400

    week 1 peer review work

---
+++ week1/poem.c
@@ -0,0 +1,31 @@
+/*  Print a Poem
+    Stopping by Woods on a Snowy Evening - By Robert Frost
+    Nick K
+    Sep 17 2021
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Whose woods these are I think I know.\n");   
+    printf("His house is in the village though\n");   
+    printf("He will not see me stopping here\n");   
+    printf("To watch his woods fill up with snow.\n\n");
+
+    printf("My little horse must think it queer\n");   
+    printf("To stop without a farmhouse near\n");
+    printf("Between the woods and frozen lake\n");
+    printf("The darkest evening of the year.\n\n");
+
+    printf("He gives his harness bells a shake\n");   
+    printf("To ask if there is some mistake.\n");
+    printf("The only other sound’s the sweep\n");
+    printf("Of easy wind and downy flake.\n\n");
+
+    printf("The woods are lovely, dark and deep,\n");
+    printf("But I have promises to keep,\n");
+    printf("And miles to go before I sleep,\n");
+    printf("And miles to go before I sleep.\n\n");
+    return 0;
+}
