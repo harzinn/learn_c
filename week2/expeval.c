a159f09450a064ac932017fdd0a9c6fb279c11ff
harzinn <nickkrugz@gmail.com>
2021-09-21 13:43:04 -0400

    evaluation expressions

---
+++ week2/expeval.c
@@ -0,0 +1,30 @@
+/*  Expression Evaluation
+    Nick K
+    Sep 21, 2021
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 5, b = 7, c = 0, d = 0; //initialize
+    c = a - b;
+    printf("a = %d, b = %d, c = %d, d = %d\n",
+        a, b, c, d);
+    c = b - a;
+    printf("a = %d, b = %d, c = %d, d = %d\n",
+        a, b, c, d);
+    c = a / b ; d = b / a;
+    printf("a = %d, b = %d, c = %d, d = %d\n",
+        a, b, c, d);
+    c = a % b ; d = b % a;
+    printf("a = %d, b = %d, c = %d, d = %d\n",
+        a, b, c, d);
+    c = -a - b ; d = -b - a;
+    printf("a = %d, b =% d, c = %d, d = %d\n",
+        a, b, c, d);
+    c = ++a + b++ ; d += 5;
+    printf("a = %d, b = %d, c = %d, d = %d\n",
+        a, b, c, d);
+    return 0;
+}
