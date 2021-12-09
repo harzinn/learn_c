76ea016751a8492195cc284a04fea351bb7e436b
harzinn <nickkrugz@gmail.com>
2021-12-08 21:29:55 -0500

    storage class intro and updated vscode preferences

---
+++ week4/storageclass.c
@@ -0,0 +1,30 @@
+/*	Storage Class 5.11
+	Nick K
+	8 Dec 2021
+	Note useful code
+*/
+
+#include <stdio.h>
+
+extern int reps =0;
+
+
+void f(void)
+{
+	static int called = 0;
+	printf("f called %d\n", called);
+	called++;
+	reps = reps + called;
+}
+
+int main(void)
+{
+	auto int i = 1;
+	const int Limit = 10;
+	for ( i = 1; i < Limit; i++)
+	{
+		printf("i local = %d, reps global =%d\n", i, reps);
+		f();
+	}
+	return 0;
+}
